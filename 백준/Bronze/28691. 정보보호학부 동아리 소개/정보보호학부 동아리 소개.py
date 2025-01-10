4402bbdaf8590253f6f84f737511fcc11a11682e
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-01-10 15:49:14 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+X = input()
+
+if X == 'M':
+    print("MatKor")
+elif X == 'W':
+    print("WiCys")
+elif X == 'C':
+    print("CyKor")
+elif X == 'A':
+    print("AlKor")
+else:
+    print("$clear")
