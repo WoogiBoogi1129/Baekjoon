c970f70f35b014662e4298e262da8ca37d221c2a
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-01-14 14:26:54 +0900

    [Bronze IV] Title: 지폐 세기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30031. 지폐 세기/지폐 세기.py
@@ -0,0 +1,14 @@
+T = int(input())
+sum = 0
+for i in range(T):
+    A, B = map(int, input().split())
+    if A == 136:
+        sum += 1000
+    elif A == 142:
+        sum += 5000
+    elif A == 148:
+        sum += 10000
+    elif A == 154:
+        sum += 50000
+
+print(sum)
