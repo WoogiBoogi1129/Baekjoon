fcdd25a161eaa413d1ef0370febc1cdd961bd164
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-01-17 13:46:08 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
@@ -1,10 +1,6 @@
 while True:
     try:
         A, B, C = map(int, input().split())
-        if C-B >= B-A:
-            result = C-B-1
-        else:
-            result = B-A-1
-        print(result)
+        print(max(B-A, C-B)-1)
     except:
         break
