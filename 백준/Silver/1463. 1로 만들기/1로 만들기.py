3762acaa84ff66455b22d07bbd55c5f9665e22b5
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-02-05 15:11:04 +0900

    [Silver III] Title: 1로 만들기, Time: 484 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,12 @@
+X = int(input())
+
+dp = [0] * 1000001
+
+for i in range(2, X+1):
+    dp[i] = dp[i-1] + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i//3] + 1, dp[i])
+    if i % 2 == 0:
+        dp[i] = min(dp[i//2] + 1, dp[i])
+
+print(dp[X])
