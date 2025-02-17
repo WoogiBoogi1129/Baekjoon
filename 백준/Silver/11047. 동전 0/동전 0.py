260e87b7b7631ac4eac8195be904a677bfeda375
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-02-17 15:49:32 +0900

    [Silver IV] Title: 동전 0, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+
+coin = []
+
+for _ in range(N):
+    coin.append(int(input()))
+    
+cnt = 0
+
+for i in range(len(coin)-1, -1, -1):
+    if coin[i] <= K:
+        cnt += K // coin[i]
+        K = K % coin[i]
+            
+print(cnt)
