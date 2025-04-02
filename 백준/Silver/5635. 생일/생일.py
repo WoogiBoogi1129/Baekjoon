447de12c803d884295292c543329e2076d03f218
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-04-02 10:33:22 +0900

    [Silver V] Title: 생일, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+
+for _ in range(n):
+    n, d, m, y = input().split()
+    d, m, y = map(int, (d, m, y))
+    arr.append((y, m, d, n))
+
+arr.sort()
+print(arr[-1][3])
+print(arr[0][3])
