a2c5c19c238b60c64c09415478eb2ff05ff949b9
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-04-09 19:24:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,13 @@
+while True:
+    a, b, c = map(int, input().split())
+    if a == b == c == 0:
+        break
+    arr = []
+    arr.append(a)
+    arr.append(b)
+    arr.append(c)
+    arr.sort()
+    if arr[-1]**2 == arr[0]**2 + arr[1]**2:
+        print("right")
+    else:
+        print("wrong")
