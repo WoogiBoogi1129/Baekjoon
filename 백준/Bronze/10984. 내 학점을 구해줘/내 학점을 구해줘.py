41c7893956a2b0bb0f324a68ac61c99930b96319
H.Taeuk <hwoogiboogi1129@gmail.com>
2025-04-17 11:45:46 +0900

    [Bronze III] Title: 내 학점을 구해줘, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10984. 내 학점을 구해줘/내 학점을 구해줘.py
@@ -0,0 +1,13 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    total_credit = 0
+    total_grade = 0
+    
+    for _ in range(N):
+        credit, grade = map(float, input().split())
+        total_credit += credit
+        total_grade += (credit * grade)
+        
+    GPA = total_grade / total_credit
+    print("%i %.1f" % (total_credit, GPA))
